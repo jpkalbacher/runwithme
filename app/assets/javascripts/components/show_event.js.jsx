e76e17a7fc641c9abcda35a2187e682c64a62af6
Jeff <jpkalbacher@gmail.com>
2015-10-14 17:55:43 -0700

    fix show event

--- app/assets/javascripts/components/show_event.js.jsx
+++ app/assets/javascripts/components/show_event.js.jsx
@@ -1,17 +1,13 @@
 var ShowEvent = React.createClass({
-
-
-  getInitialState: function () {
-    var eventId = this.props.params.eventId;
-    var this_event = this._findEventById(eventId) || {} ;
-    return { this_event: this_event };
+    getInitialState: function () {
+    return { event: _findEventById(this.props.params.eventId) };
   },
 
   _findEventById: function (id) {
-    var res;
+    var res = {};
       EventStore.all().forEach(function (event) {
-        if (id == this_event.id) {
-          res = this_event;
+        if (id == event.id) {
+          res = event;
         }
     }.bind(this));
     return res;
@@ -19,20 +15,26 @@ var ShowEvent = React.createClass({
 
   componentDidMount: function () {
     EventStore.addChangeListener(this._eventChanged);
+    ApiUtil.fetchSingleEvent(this.props.params.eventId);
+  },
+
+  componentWillMount: function () {
     ApiUtil.fetchInBounds();
+    EventStore.addChangeListener(this._eventChanged);
   },
 
   _eventChanged: function () {
     var eventId = this.props.params.eventId;
-    var this_event = this._findEventById(eventId);
-    this.setState({ this_event: this_event });
+    var event = this._findEventById(eventId);
+    this.setState({ event: event });
   },
 
   render: function() {
+    debugger;
     return (
       <div>
         <h1>HELLO</h1>
-        {this.props.event_type}
+        {this.state.event.whateverahdahad}
         {this.props.location_description}
       </div>
     )
